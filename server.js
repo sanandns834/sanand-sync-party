3960bb8b30f4f881a865fcb97145d5cf56fc62a9
sanandns834 <sanandns834@gmail.com>
2026-02-26 11:41:01 +0530

    server.js

--- server.js
+++ server.js
@@ -10,44 +10,58 @@ app.use(express.static(__dirname));
 
 let rooms = {};
 
+function generateRoomCode() {
+  return Math.random().toString(36).substring(2, 7).toUpperCase();
+}
+
 io.on("connection", (socket) => {
 
-    socket.on("createRoom", () => {
-        const code = Math.floor(1000 + Math.random() * 9000).toString();
-        rooms[code] = { users: 1 };
-        socket.join(code);
-        socket.roomCode = code;
-        socket.emit("roomCreated", code);
-        io.to(code).emit("updateUsers", rooms[code].users);
-    });
+  socket.on("createRoom", () => {
+    const code = generateRoomCode();
+    rooms[code] = {
+      host: socket.id,
+      users: []
+    };
 
-    socket.on("joinRoom", (code) => {
-        if (rooms[code]) {
-            rooms[code].users++;
-            socket.join(code);
-            socket.roomCode = code;
-            socket.emit("joinedRoom", code);
-            io.to(code).emit("updateUsers", rooms[code].users);
+    socket.join(code);
+    rooms[code].users.push(socket.id);
+
+    socket.emit("roomCreated", code);
+    io.to(code).emit("updateUsers", rooms[code].users.length);
+  });
+
+  socket.on("joinRoom", (code) => {
+    if (rooms[code]) {
+      socket.join(code);
+      rooms[code].users.push(socket.id);
+
+      socket.emit("roomJoined", code);
+      io.to(code).emit("updateUsers", rooms[code].users.length);
+    }
+  });
+
+  socket.on("syncPlay", (code) => {
+    if (rooms[code] && rooms[code].host === socket.id) {
+      io.to(code).emit("playNow");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (let code in rooms) {
+      if (rooms[code]) {
+        rooms[code].users = rooms[code].users.filter(id => id !== socket.id);
+
+        if (rooms[code].users.length === 0) {
+          delete rooms[code];
+        } else {
+          io.to(code).emit("updateUsers", rooms[code].users.length);
         }
-    });
-
-    socket.on("syncPlay", (code) => {
-        io.to(code).emit("playNow");
-    });
-
-    socket.on("disconnect", () => {
-        const code = socket.roomCode;
-        if (code && rooms[code]) {
-            rooms[code].users--;
-            io.to(code).emit("updateUsers", rooms[code].users);
-        }
-    });
+      }
+    }
+  });
 
 });
 
-// IMPORTANT: For deployment (Render needs this)
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log("🚀 Sanand's Sync Party running...");
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
